a796df9345da8814b7cb59a27b9527aa9187d597
Tuchan Anthony <62402636+Tuchakage@users.noreply.github.com>
2025-05-02 16:08:18 +0100

    Player Improved
    - Player can Jump and Punch
    - Virtual Camera properties has been copied over to main camera

--- Operation-Pet/Assets/Scripts/Character Scripts/Pet/PetController.cs
+++ Operation-Pet/Assets/Scripts/Character Scripts/Pet/PetController.cs
@@ -3,11 +3,16 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Photon.Pun;
+using Cinemachine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PetController : MonoBehaviourPun
 {
+    
     InputSystem_Actions playerActionAsset;
     InputAction move;
+
+    [SerializeField]
     ThirdPersonAnimation animationScript; //Set the animation values within the script
 
     Vector2 currentMovement;
@@ -19,6 +24,8 @@ public class PetController : MonoBehaviourPun
     private Rigidbody rb;
     [SerializeField]
     private float movementForce = 1f;
+    [SerializeField]
+    private float jumpForce = 5f;
     [SerializeField]
     private float maxSpeed = 5f;
 
@@ -34,11 +41,18 @@ public class PetController : MonoBehaviourPun
 
         if (photonView.IsMine) 
         {
+            playerCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+
+            //Get access ot the CineMachine Free Look Component so that we can set what the camera needs to follow and Look At
+            CinemachineFreeLook cineMachine = playerCamera.GetComponent<CinemachineFreeLook>();
+            cineMachine.Follow = this.transform;
+            cineMachine.LookAt = this.transform;
+
             rb = GetComponent<Rigidbody>();
             playerCamera.enabled = true;
+            Cursor.lockState = CursorLockMode.Locked;
         }
 
-        animationScript = GetComponent<ThirdPersonAnimation>();
 
         //performed is a callback function and will return the current state of the callback of the player input (Stored in ctx)
         //Code Syntax for callbacks is "+ = ctx"
@@ -54,21 +68,38 @@ public class PetController : MonoBehaviourPun
 
     void OnEnable()
     {
+        //Subscribe the Jump Event to Jump function
+        playerActionAsset.Player.Jump.started += Jump;
+        playerActionAsset.Player.Attack.started += Attack;
+
         //Get the inputs for the move action
         move = playerActionAsset.Player.Move;
         //Enable the Player Action Map 
         playerActionAsset.Player.Enable();
     }
 
+
     void OnDisable()
     {
         //Disable the Player Action Map 
         playerActionAsset.Player.Disable();
+
+        //UnSubscribe the Jump Event from the Jump function
+        playerActionAsset.Player.Jump.started += Jump;
+        playerActionAsset.Player.Attack.started -= Attack;
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        animationScript.SetMaxSpeed(maxSpeed);
+        if (animationScript != null)
+        {
+            //Make sure the Max Speed for the animation is the same as the maxSpeed set in this scriptvcd
+            animationScript.SetMaxSpeed(maxSpeed);
+        }
+        else 
+        {
+            Debug.LogWarning("No Animator is attached. Please drag and drop from the Inspector");
+        }
     }
 
     // Update is called once per frame
@@ -94,6 +125,12 @@ public class PetController : MonoBehaviourPun
         //When we let go of our movement input button then the player will stop moving
         forceDirection = Vector3.zero;
 
+        //Increase acceleration as we are falling
+        if (rb.linearVelocity.y < 0f) 
+        {
+            rb.linearVelocity -= Vector3.down * Physics.gravity.y * Time.deltaTime;
+        }
+
         Vector3 horizontalVelocity = rb.linearVelocity;
         horizontalVelocity.y = 0f;
 
@@ -106,6 +143,32 @@ public class PetController : MonoBehaviourPun
         LookAt();
     }
 
+     void Jump(InputAction.CallbackContext context)
+    {
+        if (IsGrounded()) 
+        {
+            //Jump upwards
+            forceDirection += Vector3.up * jumpForce;
+        }
+    }
+
+    bool IsGrounded() 
+    {
+        //Use Raycast to check what is below us
+        // transform.position + Vector3.up * 0.25f = Makes sure we ray cast from above
+        Ray ray = new Ray(transform.position + Vector3.up * 0.25f, Vector3.down);
+
+        //Spawn in the raycast and if it hit something return true
+        if (Physics.Raycast(ray, out RaycastHit hit, 0.3f))
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+        
+    }
     Vector3 GetCameraRight(Camera camera)
     {
         Vector3 right = playerCamera.transform.right;
@@ -139,6 +202,18 @@ public class PetController : MonoBehaviourPun
             rb.angularVelocity = Vector3.zero;
         }
 
+    }
+
+    void Attack(InputAction.CallbackContext context)
+    {
+        if (animationScript != null)
+        {
+            animationScript.Attack();
+        }
+        else
+        {
+            Debug.LogWarning("Animation Script is not attached. Please drag and drop from the Inspector");
+        }
     }
 
 
